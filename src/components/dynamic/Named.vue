c457186d431286c717e5c12370a9baa688d5a6ab
LZXprogrammer <LZXprogrammer@outlook.com>
2018-02-03 13:42:14 +0000

    ...

---
+++ src/components/dynamic/Named.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="named">
+    <h1>Named Views</h1>
+    <ul>
+      <li>
+        <router-link to="/named/lib">/lib</router-link>
+      </li>
+      <li>
+        <router-link to="/named/other">/other</router-link>
+      </li>
+    </ul>
+    <router-view class="view one"></router-view>
+    <router-view class="view two" name="a"></router-view>
+    <router-view class="view three" name="b"></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Named'
+}
+</script>
+
+<style scoped>
+</style>
--- src/router/lib.js
+++ src/router/lib.js
@@ -1,4 +1,5 @@
 import User from '@/components/dynamic/User'
+import Named from '@/components/dynamic/Named'
 import Home from '@/components/Home'
 import hello from '@/components/hello'
 import HelloVue from '@/components/HelloVue'
@@ -11,7 +12,24 @@ const UserProfile = { template: '<div>Profile</div>' }
 const UserPosts = { template: '<div>Posts</div>' }
 
 
+const Foo1 = { template: '<div>foo</div>' }
+const Bar1 = { template: '<div>bar</div>' }
+const Baz1 = { template: '<div>baz</div>' }
+
+const Login = { template: '<div>Login</div>' }
+
 export default [
+  {
+    path: '/home',
+    redirect: => to {
+      name: 'Login'
+    }
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
   {
     path: '/user/:id',
     name: 'User',
@@ -59,5 +77,29 @@ export default [
     path: '/bar',
     name: 'Bar',
     component: Bar
+  },
+  {
+    path: '/named',
+    name: 'Named',
+    components: Named,
+    children: [
+      {
+        path: 'lib',
+        components: {
+          default: Foo1,
+          a: Bar1,
+          b: Baz1
+        }
+      },
+      {
+        path: 'other',
+        // name: 'Named',
+        components: {
+          default: Baz1,
+          a: Bar1,
+          b: Foo1
+        }
+      }
+    ]
   }
 ]
